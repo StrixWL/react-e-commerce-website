708ce9dda66fb313a04bdee65411e54473b87e71
StrixWL <brahimstrix@gmail.com>
2024-03-22 12:44:45 +0000

    fixed header css after installing tailwind ;-;

--- src/components/Header/UtilitySection/UtilitySection.jsx
+++ src/components/Header/UtilitySection/UtilitySection.jsx
@@ -6,16 +6,20 @@ import { Link } from "react-router-dom";
 const UtilitySection = () => {
 	return (
 		<section className={styles["utility-section"]}>
-			<button className={styles.search}>
-				<img src={searchLogo} alt="Search" />
-			</button>
+			<div className={styles["search"]}>
+				<button className={styles["search-button"]}>
+					<img src={searchLogo} alt="Search" />
+				</button>
+			</div>
 			<div className={styles["header-logo"]}>
 				<Link to="/">
 					<img src={headerLogo} alt="Logo" />
 				</Link>
 			</div>
-			<div className={styles.utilities}>
-				<img src={searchLogo} alt="Search" />
+			<div className={styles["search"]}>
+				<button className={styles["search-button"]}>
+					<img src={searchLogo} alt="Search" />
+				</button>
 			</div>
 		</section>
 	);
