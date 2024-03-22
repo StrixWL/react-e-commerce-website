4c4af49dd65c5e89323217ef2519ff3f96aca4b2
Oualid-A <oualid.achrif@ump.ac.ma>
2024-03-22 16:30:14 +0100

    feat(resources): add images and data file for enhanced product visualization and information

---
+++ src/data/data.jsx
@@ -0,0 +1,46 @@
+const productDetailItem = {
+  images: [
+    {
+      original:
+        "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600",
+      thumbnail:
+        "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg?auto=compress&cs=tinysrgb&w=600",
+      thumbnail:
+        "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/2697787/pexels-photo-2697787.jpeg?auto=compress&cs=tinysrgb&w=600",
+      thumbnail:
+        "https://images.pexels.com/photos/2697787/pexels-photo-2697787.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/3373736/pexels-photo-3373736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      thumbnail:
+        "https://images.pexels.com/photos/3373736/pexels-photo-3373736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      original:
+        "https://images.pexels.com/photos/3910071/pexels-photo-3910071.jpeg?auto=compress&cs=tinysrgb&w=600",
+      thumbnail:
+        "https://images.pexels.com/photos/3910071/pexels-photo-3910071.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+  ],
+  title: "BIG ITALIAN SOFA",
+  reviews: "150",
+  availability: true,
+  brand: "apex",
+  category: "Sofa",
+  sku: "BE45VGTRK",
+  price: 450,
+  previousPrice: 599,
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem exercitationem voluptate sint eius ea assumenda provident eos repellendus qui neque! Velit ratione illo maiores voluptates commodi eaque illum, laudantium non!",
+  size: ["XS", "S", "M", "L", "XL"],
+  color: ["gray", "violet", "red"],
+};
