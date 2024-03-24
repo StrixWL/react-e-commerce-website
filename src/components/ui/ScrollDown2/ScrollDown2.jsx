400f92117118eff8f86b9eb8b2a158572efe53a9
StrixWL <brahimstrix@gmail.com>
2024-03-24 11:42:14 +0000

    feat(ui): Add arrow animation components

---
+++ src/components/ui/ScrollDown2/ScrollDown2.jsx
@@ -0,0 +1,15 @@
+import styles from "./ScrollDown2.module.css";
+
+const ScrollDown2 = ({ className }) => {
+	return (
+		<div className={className}>
+			<div class={styles["arrow"]}>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	);
+};
+
+export default ScrollDown2;
