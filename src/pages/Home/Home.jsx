951fc65e6778e1d9743d9d6dbf2a7c0693af4fd2
StrixWL <brahimstrix@gmail.com>
2024-03-25 05:24:33 +0000

    feat(homepage): Complete implementation of ServiceShowCase component with responsive design for mobile and PC

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import styles from './Home.module.css'
 import introVid from '../../assets/introVid.webp'
 import ScrollDown1 from '../../components/ui/ScrollDown1/ScrollDown1';
 import ScrollDown2 from '../../components/ui/ScrollDown2/ScrollDown2';
+import ServiceShowCase from './ServiceShowCase/ServiceShowCase';
 
 const Home = () => {
 	return (
@@ -10,13 +11,14 @@ const Home = () => {
 				<img src={introVid} />
 				<aside className={styles['scroll']}>
 					<ScrollDown1 className={styles['scroll-btn']}/>
-					
 					{/* <ScrollDown2 className={styles['scroll-animation1']} />
 					<ScrollDown2 className={styles['scroll-animation2']} /> */}
 					
 					{/* spent so much time on this but ended up not looking so good : ( */}
 				</aside>
 			</section>
+			<ServiceShowCase />
+
 		</main>
 	);
 };
---
+++ src/pages/Home/ServiceShowCase/ServiceShowCase.jsx
@@ -0,0 +1,24 @@
+import styles from "./ServiceShowCase.module.css";
+import img from '../../../assets/imazighenhh.webp'
+import { Link } from "react-router-dom";
+
+const ServiceShowCase = () => {
+	return (
+		<div className={styles['wrapper']}>
+			<div className={styles["service-show-case"]}>
+                <div className={styles['img']}>
+                    <img src={img}/>
+                </div>
+                <div className={styles['content-wrapper']}>
+                    <div className={styles['content']}>
+                        <h2>Crafted with Precision, Embracing Tradition.</h2>
+                        <p>Our clothing is the embodiment of precision and pride. Each piece is meticulously crafted to ensure you experience the very best in quality. Beyond being mere garments, they carry the rich essence of Amazigh and Rif culture. It's more than just clothing; it's a statement of tradition and craftsmanship that resonates with the spirit of these vibrant cultures.</p>
+                        <Link to="/">Collections</Link>
+                    </div>
+                </div>
+            </div>
+		</div>
+	);
+};
+
+export default ServiceShowCase;
