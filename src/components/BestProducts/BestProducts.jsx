f58a4373ce48c33a59e2392ef832f75df5ba6865
StrixWL <brahimstrix@gmail.com>
2024-03-25 11:21:15 +0000

    feat(homepage): products on hover will show the backside picture of it now

--- src/components/BestProducts/BestProducts.jsx
+++ src/components/BestProducts/BestProducts.jsx
@@ -5,12 +5,25 @@ import Product from "./Product/Product";
 import { useState } from "react";
 
 const BestProducts = () => {
-	const [items, setItems] = useState(testItems)
+	const [items, setItems] = useState(testItems);
 	return (
 		<div className={styles["wrapper"]}>
-			<ul className={styles['items']}>
+			<ul className={styles["items"]}>
 			{testItems.map((item, i) => {
-					console.log("hh", item.images.map(i => i.original))
+					return (
+						<li key={i}>
+							<Product
+								title={item.title}
+								beforeHoverImage={item.beforeImage}
+								afterHoverImage={item.afterImage}
+								priceBefore={item.previousPrice}
+								priceAfter={item.price}
+								id={item.id}
+							/>
+						</li>
+					);
+				})}
+				{testItems.map((item, i) => {
 					return (
 						<li key={i}>
 							<Product
